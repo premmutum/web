93b480d04509ff6a6b5c32b1d7e5d7b9c3e95941
premmutum <67655153+premmutum@users.noreply.github.com>
2026-03-23 19:00:29 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -56,13 +56,10 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 //google drive link
 const images = [
-"https://drive.google.com/thumbnail?id=1pG8QChhxIkMUXYF3ZOSVx7C3EWxMxW1d&sz=w1000",
-"https://drive.google.com/thumbnail?id=1eIYMzl60-q410u4ZBGJjdOjoIkXoJUnD&sz=w1000",
-"https://drive.google.com/thumbnail?id=1hcFKLCA_R7oF1mOK3MjAzAop0-Bj8Iov&sz=w1000",
-"https://drive.google.com/thumbnail?id=1JaKvZXcTkcPt5y0QCpwk41jauVDKiy_b&sz=w1000",
+"https://drive.google.com/thumbnail?id=1fsKy5aHh2CH0VlRt9uBplkSWRhgqHLVD=w1000",
 "https://drive.google.com/thumbnail?id=1-9CikNKPrsd5VBQp4vE171I36Ik3GmyV&sz=w1000",
 "https://drive.google.com/thumbnail?id=1nh43k8SP6z1Q3QqqZgENkA39QFTHJA2n&sz=w1000",
-"https://drive.google.com/thumbnail?id=1bmiC7ZQlJ0E-xo4e06bF6a3lAGYFm-3_&sz=w1000"
+"https://drive.google.com/thumbnail?id=1bmiC7ZQlJ0E-xo4e06bF6a3lAGYFm-3_&sz=w1000",
 ];
 
 
